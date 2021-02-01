bbb01406027e4567d6477b37919fce390ef0c61e
B1rtek <skrzeczek@onet.pl>
2021-02-01 21:08:36 +0100

    kolizja między paletką a piłką

--- src/ball.js
+++ src/ball.js
@@ -1,11 +1,12 @@
 export default class Ball {
     constructor(game) {
         this.image = document.getElementById("img_ball");
-        this.speed = {x: 30, y: 30};
+        this.speed = {x: 10, y: 10};
         this.position = {x: 10, y: 10};
         this.size = 16;
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
+        this.game = game;
     }
 
     draw(context) {
@@ -15,21 +16,36 @@ export default class Ball {
     update(deltaTime) {
         this.position.x += this.speed.x / deltaTime;
         this.position.y += this.speed.y / deltaTime;
+
+        //kolizje
+        //ściany
         if (this.position.x > this.gameWidth - this.size) {
             this.position.x = this.gameWidth - this.size;
             this.speed.x *= -1;
-        }
-        if (this.position.y > this.gameHeight - this.size) {
-            this.position.y = this.gameHeight - this.size;
-            this.speed.y *= -1;
         }
         if (this.position.x < 0) {
             this.position.x = 0;
             this.speed.x *= -1;
         }
+        //sufit i podłoga
+        if (this.position.y > this.gameHeight - this.size) {
+            this.position.y = this.gameHeight - this.size;
+            this.speed.y *= -1;
+        }
         if (this.position.y < 0) {
             this.position.y = 0;
             this.speed.y *= -1;
+        }
+        //paletka
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+        if(bottomOfBall>=topOfPaddle)
+        {
+            if(this.position.x+this.size/2>=this.game.paddle.position.x && this.position.x+this.size/2<=this.game.paddle.position.x+this.game.paddle.width)
+            {
+                this.speed.y*=-1;
+                this.position.y=topOfPaddle-this.size;
+            }
         }
     }
 }
