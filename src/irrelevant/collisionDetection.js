0db0f28e066077ad513aa60cc40d5a6457f885f0
B1rtek <skrzeczek@onet.pl>
2021-02-02 23:59:24 +0100

    Porządek i poprawienie wydajności animacji

--- src/irrelevant/collisionDetection.js
+++ src/irrelevant/collisionDetection.js
@@ -7,9 +7,5 @@ export function detectCollision(ball, gameObject) {
     let rightSideOfObject = gameObject.position.x + gameObject.width;
     let bottomOfObject = gameObject.position.y + gameObject.height;
 
-    if (bottomOfBall >= topOfObject && topOfBall <= bottomOfObject && ball.position.x >= leftSideOfObject && ball.position.x + ball.size <= rightSideOfObject) {
-        return true;
-    } else {
-        return false;
-    }
+    return bottomOfBall >= topOfObject && topOfBall <= bottomOfObject && ball.position.x >= leftSideOfObject && ball.position.x + ball.size <= rightSideOfObject;
 }
